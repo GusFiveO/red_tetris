5cd597824e447450348733005e14af6626f530f6
WalterLuong <wluong@student.42.fr>
2024-02-05 19:59:43 +0100

    setting up websocket + test

--- backend/index.ts
+++ backend/index.ts
@@ -1,27 +1,58 @@
-import express from 'express'
-import cors from 'cors'
-import cookieParser from 'cookie-parser'
-import { authRouter } from "./routes/auth"
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import express from 'express';
+import { createServer } from 'http';
+import { join } from 'path';
+import { Server } from 'socket.io';
+import { authRouter } from './routes/auth';
 import { playerRouter } from './routes/player';
 
 const app = express();
 const port = process.env.BACK_PORT;
+const httpServer = createServer(app);
+const io = new Server(httpServer);
 
+// io.use(async (socket, next) => {
+//   try {
+//     if (socket.handshake.auth.token) {
+//       next();
+//     } else {
+//       const err = new Error('not authorized');
+//       next(err);
+//     }
+//   } catch (e) {
+//     next(new Error('not authorized'));
+//   }
+// });
 
 app.use(cookieParser());
-app.use(cors({
-  origin: 'http://localhost:3000',
-  credentials: true
-}))
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  })
+);
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.sendFile(join(__dirname, 'test.html'));
 });
 
-app.use('/auth', authRouter)
+app.use('/auth', authRouter);
 
-app.use('/player', playerRouter)
+app.use('/player', playerRouter);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+const server = httpServer.listen(port, () => {
+  console.log(`Example app listening on port : ${port}`);
 });
+
+// app.listen(port, () => {
