8b8d35902b2044a7e0384e896f8a1c94589591d2
Augustin LORAIN <alorain@bess-f2r1s15.clusters.42paris.fr>
2024-02-09 15:02:50 +0100

    feat(frontend/Game.tsx): better opponent spectre with ranking

--- frontend/src/pages/Game.tsx
+++ frontend/src/pages/Game.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import '../styles/custom-utilities.css';
 import {
   createMatrix,
   createRandomMatrix,
@@ -13,16 +14,16 @@ type GameProps = {
 };
 
 const opponentsSample = [
-  { name: 'Player 1', firstLine: 2 },
-  { name: 'Player 2', firstLine: 5 },
-  { name: 'Player 3', firstLine: 8 },
-  { name: 'Player 4', firstLine: 11 },
-  { name: 'Player 5', firstLine: 14 },
-  { name: 'Player 6', firstLine: 2 },
-  { name: 'Player 7', firstLine: 5 },
-  { name: 'Player 8', firstLine: 8 },
-  { name: 'Player 9', firstLine: 11 },
-  { name: 'Player 10', firstLine: 15 },
+  { name: 'Player 1', firstLine: 2, rank: 8 },
+  { name: 'Player 2', firstLine: 5, rank: 4 },
+  { name: 'Player 3', firstLine: 8, rank: 9 },
+  { name: 'Player 4', firstLine: 11, rank: 3 },
+  { name: 'Player 5', firstLine: 14, rank: 10 },
+  { name: 'Player 6', firstLine: 2, rank: 5 },
+  { name: 'Player 7', firstLine: 5, rank: 7 },
+  { name: 'Player 8', firstLine: 8, rank: 1 },
+  { name: 'Player 9', firstLine: 11, rank: 6 },
+  { name: 'Player 10', firstLine: 15, rank: 2 },
 ];
 
 type Matrix = number[][];
@@ -33,6 +34,7 @@ export const Game = ({ hash }: GameProps) => {
     {
       name: string;
       firstLine: number;
+      rank: number;
     }[]
   >(opponentsSample);
 
@@ -42,6 +44,13 @@ export const Game = ({ hash }: GameProps) => {
 
     const handleKeyPress = (event: KeyboardEvent) => {
       const currentTime = new Date().getTime();
+      if (
+        event.key == ' ' ||
+        event.key === 'ArrowDown' ||
+        event.key === 'ArrowUp'
+      ) {
+        event.preventDefault();
+      }
       if (currentTime - lastKeyPressTime > throttleInterval) {
         if (event.key === 'ArrowUp') {
           console.log(event.key);
@@ -72,28 +81,49 @@ export const Game = ({ hash }: GameProps) => {
       {row.map((elem, colIndex) => (
         <div
           key={colIndex}
-          className={`h-8 w-8 border border-black ${getColorForNumber(elem)}`}
+          className={`block ${getColorForNumber(elem)}`}
         ></div>
       ))}
     </div>
   ));
 
-  const opponentsComponent = opponents.map(({ name, firstLine }) => {
-    return (
-      <div key={name} className='flex flex-col justify-center items-center m-2'>
-        <div>{name}</div>
-        <div className='w-10 h-20 border m-1'>{firstLine}</div>
-      </div>
-    );
-  });
+  const opponentsComponent = opponents
+    .sort((a, b) => {
+      return a.rank > b.rank ? 1 : 0;
+    })
+    .map(({ name, firstLine, rank }) => {
+      const zerosMatrix = createMatrix(ROWS, COLUMNS);
+      return (
+        <div
+          key={name}
+          className='flex flex-col justify-center items-center m-2'
+        >
+          <div>{name}</div>
+          {zerosMatrix.map((row, rowIndex) => (
+            <div key={rowIndex} className='flex'>
+              {row.map((elem, colIndex) => (
+                <div
+                  key={colIndex}
+                  className={`w-1 h-1 border border-black ${
+                    rowIndex > firstLine ? 'bg-black' : 'bg-gray-600'
+                  }`}
+                ></div>
+              ))}
+            </div>
+          ))}
+
+          {/* <div className='w-10 h-20 border m-1'>{firstLine}</div> */}
+        </div>
+      );
+    });
 
   return (
     <div className='main-container flex justify-center items-center'>
       <div className='flex flex-col items-center justify-center'>
         <div>GAME : {hash}</div>
-        <div>{fieldComponent}</div>
+        <div className='border rounded-md'>{fieldComponent}</div>
       </div>
-      <div className='bg-red-600 overflow-y-scroll h-full'>
+      <div className='flex flex-col bg-slate-900 rounded-md overflow-y-scroll h-auto max-h-[75%] m-1 border'>
         {opponentsComponent}
       </div>
     </div>
