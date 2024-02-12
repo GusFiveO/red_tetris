db4c81e93de3f9a5ef46d0597d2fdbe00939e254
Augustin LORAIN <alorain@bess-f1r4s3.clusters.42paris.fr>
2024-02-12 15:01:18 +0100

    feat(frontend): responsive on scoreboad

--- frontend/src/components/Scoreboard.tsx
+++ frontend/src/components/Scoreboard.tsx
@@ -1,6 +1,6 @@
 import { ScoreInfo } from '../pages/Game';
 import '../styles/custom-utilities.css';
-import '../styles/hide-scrollbar.css'
+import '../styles/hide-scrollbar.css';
 
 type ScoreboardProps = {
   scoreList: ScoreInfo[];
@@ -12,7 +12,7 @@ export const Scoreboard = ({ scoreList }: ScoreboardProps) => {
       return a.score > b.score ? 1 : 0;
     })
     .map(({ name, score }) => (
-      <div key={name} className='flex m-2'>
+      <div key={name} className='flex m-2 md:text-base text-xs'>
         {`${name.padEnd(12, '\u00A0')}:  ${score
           .toString()
           .padStart(4, '\u00A0')}`}
