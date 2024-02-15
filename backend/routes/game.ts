cf728ed48ac2f1861f77ce2c56a9a86367b49652
WalterLuong <wluong@student.42.fr>
2024-02-15 17:53:14 +0100

    fix(game): improve game logic

--- backend/routes/game.ts
+++ backend/routes/game.ts
@@ -1,23 +1,31 @@
 import express from 'express';
 import path from 'path';
 import GameService from '../services/gameService';
+import gameService2 from '../services/gameService2';
 
 export const gameRouter = express.Router();
 
 const game = new GameService();
+const game2 = new gameService2();
 
 const publicPath = path.join(__dirname, '../services');
 
 game.startGame();
 
 setInterval(() => {
-  game.moveDown();
-  // game.updateGame();
-  // console.log(game.gameStatus());
+  game2.moveDown();
+  game2.updateGame();
+  console.log(game2.gameStatus());
   // console.log(game.actualTetriminoStatus());
 }, 1000);
 
 gameRouter.get('/', (req, res) => {
   const htmlFilePath = path.join(publicPath, 'index.html');
   res.sendFile(htmlFilePath);
+});
+
+gameRouter.get('/2', (req, res) => {
+  let g = game2.gameLoop();
+  // console.log(g);
+  res.send(game2.gameLoop());
 });
--- backend/services/gameService2.ts
+++ backend/services/gameService2.ts
@@ -64,10 +64,135 @@ class gameService2 {
     if (this.tetrominoSequence.length === 0) {
       this.createTetrominoList();
     }
-    if (this.tetrominoSequence.length) {
-      const pieceName = this.tetrominoSequence.pop();
-      const piece = pieceName ? this.tetrominoList[pieceName] : undefined;
+    const pieceName = this.tetrominoSequence.pop();
+    const piece = pieceName
+      ? this.tetrominoList[pieceName]
+      : this.tetrominoList[0];
+    const col = this.gameBoard[0].length / 2 - Math.ceil(piece[0].length / 2);
+
+    const row = pieceName === 'I' ? -1 : -2;
+    return { piece, col, row, pieceName };
+  };
+
+  private rotate = (piece: number[][]) => {
+    const len = piece.length - 1;
+    const roatedPiece = piece.map((row, i) =>
+      row.map((val, j) => piece[len - j][i])
+    );
+    return roatedPiece;
+  };
+
+  private checkCollision = (piece: number[][], row: number, col: number) => {
+    for (let i = 0; i < piece.length; i++) {
+      for (let j = 0; j < piece[i].length; j++) {
+        if (
+          piece[i][j] &&
+          (col + j < 0 ||
+            col + j >= this.gameBoard[0].length ||
+            row + i >= this.gameBoard.length)
+        ) {
+          return false;
+        }
+      }
     }
+    return true;
+  };
+
+  private currentPiece = this.getNextPiece();
+
+  private placePiece = () => {
+    for (let i = 0; i < this.currentPiece.piece.length; i++) {
+      for (let j = 0; j < this.currentPiece.piece[i].length; j++) {
+        if (this.currentPiece.piece[i][j]) {
+          if (this.currentPiece.row + i < 0) {
+            // game over function
+            return false;
+          }
+          this.gameBoard[this.currentPiece.row + i][this.currentPiece.col + j] =
+            this.currentPiece.piece[i][j];
+        }
+      }
+    }
+    for (let i = 0; i < this.gameBoard.length - 1; i >= 0) {
+      if (this.gameBoard[i].every((val) => !!val)) {
+        for (let r = i; r > 0; r--) {
+          for (let c = 0; c < this.gameBoard[r].length; c++) {
+            this.gameBoard[r][c] = this.gameBoard[r - 1][c];
+          }
+        }
+      } else {
+        i--;
+      }
+    }
+    this.currentPiece = this.getNextPiece();
+    return true;
+  };
+
+  public gameLoop = () => {
+    this.placePiece();
+    if (this.currentPiece) {
+      if (
+        !this.checkCollision(
+          this.currentPiece.piece,
+          this.currentPiece.row,
+          this.currentPiece.col
+        )
+      ) {
+        this.currentPiece.row--;
+        this.placePiece();
+      }
+    }
+  };
+
+  public moveDown = () => {
+    const nextRow = this.currentPiece.row + 1;
+    if (
+      !this.checkCollision(
+        this.currentPiece.piece,
+        nextRow,
+        this.currentPiece.col
+      )
+    ) {
+      this.currentPiece.row = nextRow - 1;
+      this.placePiece();
+    } else {
+      this.currentPiece.row = nextRow;
+    }
+  };
+
+  public moveLeft = () => {
+    const nextCol = this.currentPiece.col - 1;
+    if (
+      this.checkCollision(
+        this.currentPiece.piece,
+        this.currentPiece.row,
+        nextCol
+      )
+    ) {
+      this.currentPiece.col = nextCol;
+    }
+  };
+
+  public moveRight = () => {
+    const nextCol = this.currentPiece.col + 1;
+    if (
+      this.checkCollision(
+        this.currentPiece.piece,
+        this.currentPiece.row,
+        nextCol
+      )
+    ) {
+      this.currentPiece.col = nextCol;
+    }
+  };
+
+  public updateGame = () => {
+    this.gameLoop();
+    return this.gameBoard;
+  };
+
+  public gameStatus = () => {
+    return this.gameBoard;
   };
 }
 
